3eac2e0310f7fa949a832caa5ce4314e1ec32bc3
Thomas <thomas_mcardle@outlook.com>
2026-05-23 21:46:28 +0100

    Fixed search bar issue

--- components/searchBar/page.tsx
+++ components/searchBar/page.tsx
@@ -9,13 +9,16 @@ export default function SearchBar() {
     }
 
     return (
-    <form>
+    <form className="flex w-full" onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const query = formData.get("search") as string;
+        handleSearch(query);
+    }}>
         <input
             className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
             placeholder="What are you looking for?" 
-            onChange={(e) => {
-                handleSearch(e.target.value);
-            }}
+            name="search"
         ></input>
     </form>
   );
