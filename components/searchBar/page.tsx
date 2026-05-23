a5f8fb707eb5d0f25f403b7fee6a34aa7a4d1b9e
Thomas <thomas_mcardle@outlook.com>
2026-05-23 21:13:06 +0100

    Improved padding off search bar.

--- components/searchBar/page.tsx
+++ components/searchBar/page.tsx
@@ -2,8 +2,8 @@
 export default function SearchBar() {
   return (
     <input
-      className="flex w-full"
-      placeholder="What are you looking for?"
+      className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
+      placeholder="What are you looking for?" 
     ></input>
   );
 }
