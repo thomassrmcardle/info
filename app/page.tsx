67a4324b4abcc9f310a81177c6b60b6d07c505d5
Thomas <thomas_mcardle@outlook.com>
2026-05-23 21:04:01 +0100

    Added search bar reusability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
+import SearchBar from "../components/SearchBar";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         
         <h1>Info.</h1>
-        <input className="flex flex-1 w-full py-12 px-16" placeholder="What are you looking for?"></input>
+        <SearchBar />
 
       </main>
     </div>
---
+++ components/searchBar/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SearchBar() {
+  return (
+    <input
+      className="flex w-full"
+      placeholder="What are you looking for?"
+    ></input>
+  );
+}
