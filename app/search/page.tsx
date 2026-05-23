18122db83835a5cc4266bf71970e250b4bccbb48
Thomas <thomas_mcardle@outlook.com>
2026-05-23 22:20:18 +0100

    Added basic search results page.

---
+++ app/search/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+import SearchBar from "../../components/searchBar/page";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
+        <div className="mb-8 w-full">
+            <SearchBar />
+        </div>
+        <p>Idk what ur wanting from me brochacho</p>
+
+      </main>
+    </div>
+  );
+}
