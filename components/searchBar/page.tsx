70a02b0327a6c93c43014a32a20fe39c76bfd7f0
Thomas <thomas_mcardle@outlook.com>
2026-05-23 23:45:38 +0100

    Fixed problem with search bar logic

--- components/searchBar/page.tsx
+++ components/searchBar/page.tsx
@@ -10,20 +10,18 @@ export default function SearchBar({ searchQuery }: SearchBarProps) {
         router.push(`/search?q=${encodeURIComponent(query)}`);
     }
 
-    const initialQuery = searchQuery || "";
-
     return (
     <form className="flex w-full" onSubmit={(e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const query = formData.get("search") as string;
-        handleSearch(searchQuery);
+        handleSearch(query);
     }}>
         <input
             className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
             placeholder="What are you looking for?" 
             name="search"
-            defaultValue={initialQuery}
+            defaultValue={searchQuery}
         ></input>
     </form>
   );
