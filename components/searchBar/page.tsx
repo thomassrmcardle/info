9c28ba8381fadc2d59174579d3b0229747e2be0c
Thomas <thomas_mcardle@outlook.com>
2026-05-23 21:35:32 +0100

    Added metadata and basic search logic.

--- components/searchBar/page.tsx
+++ components/searchBar/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import {useRouter} from "next/navigation"; 
 
 export default function SearchBar() {
-  return (
+    const router = useRouter();
+
+    function handleSearch(query: string) {
+        router.push(`/search?q=${encodeURIComponent(query)}`);
+    }
+
+    return (
     <input
       className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
       placeholder="What are you looking for?" 
+      onChange={(e) => {
+        handleSearch(e.target.value);
+      }}
     ></input>
   );
 }
