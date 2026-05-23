1644185a6d09f210b1db44bf5df0ecaa7f74343f
Thomas <thomas_mcardle@outlook.com>
2026-05-23 21:39:59 +0100

    Fixed a couple of bugs

--- components/searchBar/page.tsx
+++ components/searchBar/page.tsx
@@ -9,12 +9,14 @@ export default function SearchBar() {
     }
 
     return (
-    <input
-      className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
-      placeholder="What are you looking for?" 
-      onChange={(e) => {
-        handleSearch(e.target.value);
-      }}
-    ></input>
+    <form>
+        <input
+            className="flex w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-4 px-6"
+            placeholder="What are you looking for?" 
+            onChange={(e) => {
+                handleSearch(e.target.value);
+            }}
+        ></input>
+    </form>
   );
 }
