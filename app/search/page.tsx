6902ac81c7825e55df2ba0eb68758f3932e974a3
Thomas <thomas_mcardle@outlook.com>
2026-05-23 23:41:00 +0100

    Fixed issues with sesrch bar

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -13,7 +13,7 @@ export default function Home({ searchParams }: SearchPageProps) {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         
         <div className="mb-8 w-full">
-            <SearchBar query={searchParams.q} />
+            <SearchBar searchQuery={searchParams.q} />
         </div>
         <p>Showing results for "{searchParams.q}"</p>
 
