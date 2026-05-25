146427f6e27f023ed54cc43c2a2304e06fcf2e08
Thomas <thomas_mcardle@outlook.com>
2026-05-25 20:03:36 +0100

    Added loader to results page.

--- components/resultsList/page.tsx
+++ components/resultsList/page.tsx
@@ -9,6 +9,8 @@ export default function ResultList() {
     
     const [value, setValue] = useState(query);
     const [results, setResults] = useState<any[]>([]);
+
+    const [loading, setLoading] = useState(false);
     
     useEffect(() => {
         setValue(query);
@@ -36,26 +38,56 @@ export default function ResultList() {
     useEffect(() => {
         let mounted = true;
         (async () => {
+            setLoading(true);
             const res = await getResults(value);
-            if (mounted) setResults(res ?? []);
+            if (mounted) {
+                setResults(res ?? []);
+                setLoading(false);
+            }
         })();
         return () => { mounted = false; };
     }, [value]);
 
+
+    function resultsStack() {
+        if (loading == true) {
+            return (
+                <div className="w-full flex flex-col items-center justify-center">
+                    {[1, 2, 3, 4, 5].map((index) => (
+                        <div key={index} className="w-full mb-4 p-4 border rounded flex flex-col card animate-pulse">
+                            <div className="w-full flex flex-row justify-start items-center">
+                                <div className="h-4 w-1/4 rounded bg-slate-200"></div>
+                            </div>
+                            <div className="mt-4 h-4 w-full rounded bg-slate-200"></div>
+                            <div className="mt-2 h-4 w-3/4 rounded bg-slate-200"></div>
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="w-full flex flex-col items-center justify-start">
+                    {results.map((result : any, index : number) => (
+                        <a className="w-full" href={`https://${result.url}`} rel="noopener noreferrer">
+                            <div key={index} className="w-full mb-4 p-4 border rounded flex flex-col card">
+                                <div className="w-full flex flex-row justify-start items-center">
+                                    <img src={`https://www.google.com/s2/favicons?domain=${result.url}`} alt="favicon" className="w-4 h-4 mr-2" />
+                                    <p className="text-blue-500">{result.title}</p>
+                                </div>
+                                {result.description && <p className="mt-1">{result.description}</p>}
+                                <p className="text-gray-600 text-sm">{result.url}</p>
+                            </div>
+                        </a>
+                    ))}
+                </div>
+            );
+        }
+    }
+
     return (
         <div className="w-full flex flex-col items-center justify-start">
-            {results.map((result : any, index : number) => (
-                <a className="w-full" href={`https://${result.url}`} rel="noopener noreferrer">
-                    <div key={index} className="w-full mb-4 p-4 border rounded flex flex-col card">
-                        <div className="w-full flex flex-row justify-start items-center">
-                            <img src={`https://www.google.com/s2/favicons?domain=${result.url}`} alt="favicon" className="w-4 h-4 mr-2" />
-                            <p className="text-blue-500">{result.title}</p>
-                        </div>
-                        {result.description && <p className="mt-1">{result.description}</p>}
-                        <p className="text-gray-600 text-sm">{result.url}</p>
-                    </div>
-                </a>
-            ))}
+            {resultsStack()}
         </div>
     );
 
